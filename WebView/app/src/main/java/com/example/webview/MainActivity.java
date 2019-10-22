23275342cb4e75a04dbed15b900d1ae85d12379a
Anisha-Jindal <anishajindal29@gmail.com>
2019-10-22 15:13:33 +0530

    Code for Web View

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView web=findViewById(R.id.webview);
+        web.loadUrl("https://www.google.com");
+    }
+}
